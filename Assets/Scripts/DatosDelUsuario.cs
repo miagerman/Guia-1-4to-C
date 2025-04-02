65fc8d27d9486724fa016193989a7c487fc04955
miagerman <49194476@est.ort.edu.ar>
2025-04-02 12:01:03 -0300

    tuve que hacer muchos cambios pero creo que esta

---
+++ Assets/Scripts/DatosDelUsuario.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DatosDelUsuario : MonoBehaviour
+{
+    public string nombreUsuario;
+    public int edadUsuario;
+    public float estaturaUsuario;
+    public bool esDonante;
+
+
+    void Start()
+    {
+        Debug.Log("Mi nombre es " + nombreUsuario);
+        Debug.Log("Tengo " + edadUsuario + " años");
+        Debug.Log("Mido " + estaturaUsuario + " metros");
+        Debug.Log("Es " + esDonante + " que soy donante de órganos");
+    }
+}
+
+
+
--- Assets/Scripts/DeclaracionDeVariables.cs
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -1,30 +1,24 @@
-﻿using System.Collections;
+﻿ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class DeclaracionDeVariables : MonoBehaviour
 {
-    int cantidadAlumnos;
-    float kilosVendidos;
-    string nombreAlumno;
-    bool estaAprobado;
+    int cantidadAlumnos = 30;
+    float kilosVendidos = 12.5f;
+    string nombreAlumno = "Mateo";
+    bool estaAprobado = true;
 
-    // Start is called before the first frame update
+
     void Start()
     {
-        cantidadAlumnos = 32;
-        kilosVendidos = 23.7f;
-        nombreAlumno = "Jerónimo";
-        estaAprobado = false;
-        Debug.Log(cantidadAlumnos);
-        Debug.Log(kilosVendidos);
-        Debug.Log(nombreAlumno);
-        Debug.Log(estaAprobado);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Cantidad de Alumnos: " + cantidadAlumnos);
+        Debug.Log("Kilos Vendidos: " + kilosVendidos);
+        Debug.Log("Nombre Alumno: " + nombreAlumno);
+        Debug.Log("Esta aprobado: " + estaAprobado);
     }
 }
+
+
+

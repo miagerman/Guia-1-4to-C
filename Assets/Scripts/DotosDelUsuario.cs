45e586eb6f24e2c19ef27a4092abccd0e39e9ff1
miagerman <49194476@est.ort.edu.ar>
2025-04-03 11:25:42 -0300

    2

--- Assets/Scripts/DotosDelUsuario.cs
+++ Assets/Scripts/DotosDelUsuario.cs
@@ -12,7 +12,12 @@ public class DotosDelUsuario : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Mi nombre es " + nombreUsuario);
+        Debug.Log("Mi nombre es " + nombreUsuario +
+                  ", tengo " + edadUsuario +
+                  " años, mido " + alturaUsuario +
+                  " metros y " + esDonante +
+                  " soy donante de órganos.");
+
     }
 
     // Update is called once per frame
